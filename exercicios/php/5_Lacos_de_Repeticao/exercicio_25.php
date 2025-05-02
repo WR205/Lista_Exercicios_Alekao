795345f5f6cd9087a664c126192b8d7a063f9466
Gabriel-M-Coutinho <gabrielshot1@gmail.com>
2025-05-02 20:10:25 +0000

    exercicio 25 php feito

--- exercicios/php/5_Lacos_de_Repeticao/exercicio_25.php
+++ exercicios/php/5_Lacos_de_Repeticao/exercicio_25.php
@@ -7,10 +7,21 @@
     <title>Exercicio 25 - Consistência</title>
 </head>
 <body>
+<form action="" method="post">
+    <span>Informe sua idade (valores aceitos de 5 a 150):</span>
+    <input type="text" name="idade" placeholder="Valor" >
+    <button type="submit">Enviar</button>
+</form>
+
     <?php
-        //  Laços de Repetição
-        //Exercicio: Consistência
-        //Autor: Seu Nome
+    if (isset($_POST['idade'])) {
+        $idade = $_POST['idade'];
+        if ($idade >= 5 && $idade <= 150) {
+            echo "<h2>Idade válida: " . $idade . "</h2>";
+        } else {
+            echo "<h2>Idade inválida. Por favor, insira um valor entre 5 e 150.</h2>";
+        }
+    }
     ?>
 </body>
 </html>
