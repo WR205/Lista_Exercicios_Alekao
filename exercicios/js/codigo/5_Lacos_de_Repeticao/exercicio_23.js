d6b163e78a31b6724f48180202fa5186f144abdd
Gabriel-M-Coutinho <gabrielshot1@gmail.com>
2025-05-02 18:49:36 +0000

    feito exercicio 23 e 24 em js e php

--- exercicios/js/codigo/5_Lacos_de_Repeticao/exercicio_23.js
+++ exercicios/js/codigo/5_Lacos_de_Repeticao/exercicio_23.js
@@ -4,4 +4,44 @@
 
 //=========================| Código |=========================//
 
+const contadorElement = document.querySelector('.contador');
+const botaoElement = document.querySelector('.detonacao');
+const explosaoElement = document.querySelector('.explosao');
+function sleep(seconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, seconds * 1000);  
+    });
+}
+
+
+
+async function detonacao(){
+    botaoElement.style.display = "none";
+    let cont = 10;
+    contadorElement.textContent = cont;
+    while(cont > 0){
+        cont -= 1;
+        contadorElement.textContent = cont;
+        sleep(1);
+        await sleep(1); 
+        
+    }
+    explodir();
+}
+
+async function explodir(){
+    contadorElement.style.display = "none";
+    explosaoElement.style.display = "flex"
+}
+
+
+
+
+
+
+
+
+
+
+
 //============================================================//
--- exercicios/php/5_Lacos_de_Repeticao/exercicio_24.php
+++ exercicios/php/5_Lacos_de_Repeticao/exercicio_24.php
@@ -7,10 +7,21 @@
     <title>Exercicio 24 - Tabuada</title>
 </head>
 <body>
+    
+    <form method="POST" action="">
+        <span>informe um numero para ver sua tabuada</span>
+        <input type="text" name="numero"></input>
+        <button type="submit">Calcular Tabuada</button>
+    </form>
+
     <?php
-        //  Laços de Repetição
-        //Exercicio: Tabuada
-        //Autor: Seu Nome
+        if (isset($_POST['numero'])) {
+            $numero = $_POST['numero'];
+            echo "<h2>Tabuada do " . $numero . "</h2>";
+            for ($i = 1; $i <= 10; $i++) {
+                echo $numero . " x " . $i . " = " . ($numero * $i) . "<br>";
+            }
+        }
     ?>
 </body>
 </html>
