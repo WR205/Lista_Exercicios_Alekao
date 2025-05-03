0e2fe6e20e9f42e09a27f5ca9352bc8501c31bbe
Gabriel-M-Coutinho <gabrielshot1@gmail.com>
2025-05-03 02:20:43 +0000

    feito exercicio 27 js

--- exercicios/js/codigo/5_Lacos_de_Repeticao/exercicio_27.js
+++ exercicios/js/codigo/5_Lacos_de_Repeticao/exercicio_27.js
@@ -4,4 +4,26 @@
 
 //=========================| Código |=========================//
 
+
+function calcularSoma() {
+    const input = document.getElementById("numero");
+    const resultado = document.getElementById("resultado");
+
+    let numero = parseInt(input.value);
+
+    if (isNaN(numero) || numero < 1) {
+        resultado.textContent = "Por favor, digite um número inteiro positivo maior que zero.";
+        return;
+    }
+
+    let soma = 0;
+
+    for (let i = 1; i <= numero; i++) {
+        soma += i;
+    }
+
+    resultado.textContent = `A soma de 1 até ${numero} é: ${soma}`;
+}
+
+
 //============================================================//
