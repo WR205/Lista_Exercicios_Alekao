abeaf0d70a29f8e91ad3c777712cd7be8eae22d2
Pedro Henrique do Vale Mineo <pdrmineo@gmail.com>
2025-05-03 19:44:55 -0300

    exercicios 2 e 3 em js

--- exercicios/js/codigo/1_Entrada_e_Saida/exercicio_02.js
+++ exercicios/js/codigo/1_Entrada_e_Saida/exercicio_02.js
@@ -1,7 +1,17 @@
 //  Entrada e Saída
 //Exercicio: Número Digitado
-//Autor: Seu Nome
+//Autor: Pedro 
 
 //=========================| Código |=========================//
+
+// Programa em Node.js que lê um número inteiro do usuário e exibe
+
+const read = require('readline-sync');
+let n1 = read.questionInt('Digite um número inteiro: ');
+
+console.log(`O número digitado foi: ${n1}`);
+
+
+
 
 //============================================================//
--- exercicios/js/codigo/1_Entrada_e_Saida/exercicio_03.js
+++ exercicios/js/codigo/1_Entrada_e_Saida/exercicio_03.js
@@ -1,7 +1,10 @@
 //  Entrada e Saída
 //Exercicio: Seu Nome
-//Autor: Seu Nome
+//Autor: Pedro
 
 //=========================| Código |=========================//
+const read = require('readline-sync');
+let nome = read.question('Digite seu nome: ');
+console.log(`Olá, ${nome}! Seja bem-vindo(a)!`);
 
 //============================================================//
