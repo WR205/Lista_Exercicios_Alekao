08b62e998eef28cbab912dff8474cd80fb03bddf
WR205 <fastbluestudios2.0@gmail.com>
2025-05-04 23:37:49 -0300

    Commit fazendo exercicio 18 js

--- exercicios/js/codigo/4_Desvios_Condicionais/exercicio_18.js
+++ exercicios/js/codigo/4_Desvios_Condicionais/exercicio_18.js
@@ -1,7 +1,34 @@
 //  Desvios Condicionais
 //Exercicio: Média Final
-//Autor: Seu Nome
+//Autor: Weverton
 
 //=========================| Código |=========================//
+
+const notas = [];
+
+const inputNota = document.getElementById("nota");
+inputNota.addEventListener("keyup", (event)=>{
+    if(event.key == "Enter"){
+        adicionarNota();
+    }
+})
+
+function adicionarNota(){
+    if(!inputNota.value){
+        alert("Insira um Valor!");
+        return;
+    }
+    if(inputNota.value < 0 || 10 < inputNota.value){
+        alert("Só é aceito notas de 0 a 10!");
+        return;
+    }
+    notasTamanho = notas.length;
+    notas.push(inputNota.value);
+    //"<p>Nota [Indice]: notas[ultimoValor]</p>(hover:<button>Deletar Elemento</button>)"
+    const novaNota = document.createElement("p");
+    novaNota.textContent = "Nota " + (notasTamanho + 1) + ": " + notas[notasTamanho];
+    document.body.appendChild(novaNota);
+    inputNota.value = "";
+}
 
 //============================================================//
