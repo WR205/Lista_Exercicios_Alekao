ddf547e2aa0a09f194eb081dd28d1ba5e778d233
WR205 <fastbluestudios2.0@gmail.com>
2025-05-04 22:56:53 -0300

    Commit exercicio 17 js feito

--- exercicios/js/codigo/4_Desvios_Condicionais/exercicio_17.js
+++ exercicios/js/codigo/4_Desvios_Condicionais/exercicio_17.js
@@ -1,7 +1,35 @@
 //  Desvios Condicionais
 //Exercicio: Números Iguais
-//Autor: Seu Nome
+//Autor: Weverton
 
 //=========================| Código |=========================//
+
+function randomNumber(max = 6){
+    return Math.floor(Math.random()*(max+1));
+}
+
+const escopo = 6;
+
+const mensagem = document.getElementById("mensagem");
+const mensagemNumeroGerado = document.getElementById("numeroGerado");
+const inputNumero = document.getElementById("numero");
+const verificarNumero = document.getElementById("verificarNumero");
+verificarNumero.addEventListener('click', ()=>{
+    if(!inputNumero.value){
+        alert("Insira um Valor!");
+        return;
+    }
+    if(escopo < inputNumero.value || inputNumero.value < 0){
+        alert("Valor informado está fora do escopo!");
+        return;
+    }
+    const numeroGerado = randomNumber(escopo);
+    mensagemNumeroGerado.innerHTML = "Número Gerado: " + numeroGerado;
+    let resultado = "diferentes";
+    if(numeroGerado == inputNumero.value){
+        resultado = "iguais";
+    }
+    mensagem.innerHTML = "Os números são " + resultado;
+})
 
 //============================================================//
