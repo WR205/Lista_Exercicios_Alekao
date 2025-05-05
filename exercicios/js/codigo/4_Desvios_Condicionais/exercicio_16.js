fa34b91719705d74d9d8348a1fc4633581f373cc
WR205 <fastbluestudios2.0@gmail.com>
2025-05-04 21:50:16 -0300

    Commit corrigindo o link css dos exercicios em js e fazendo o exercicio 1 e 16 em js

--- exercicios/js/codigo/4_Desvios_Condicionais/exercicio_16.js
+++ exercicios/js/codigo/4_Desvios_Condicionais/exercicio_16.js
@@ -1,7 +1,22 @@
 //  Desvios Condicionais
 //Exercicio: Múltiplo de Cinco
-//Autor: Seu Nome
+//Autor: Weverton
 
 //=========================| Código |=========================//
+
+const mensagem = document.getElementById("mensagem");
+const inputNumero = document.getElementById("numero");
+const verificarNumero = document.getElementById("verificarNumero");
+verificarNumero.addEventListener('click', ()=>{
+    if(!inputNumero.value){
+        alert("Insira um Valor!");
+        return;
+    }
+    let resultado = "não"
+    if((inputNumero.value % 5) == 0){
+        resultado = "";
+    }
+    mensagem.innerHTML = "<strong>" + inputNumero.value + "</strong> " + resultado + " é multiplo de cinco"
+})
 
 //============================================================//
