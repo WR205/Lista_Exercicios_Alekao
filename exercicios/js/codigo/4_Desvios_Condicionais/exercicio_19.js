ae9a6cefc1979da6d23b8b2fac3c4e4932801ef6
WR205 <fastbluestudios2.0@gmail.com>
2025-05-07 17:33:04 -0300

    Commit exercicio 19 feito

--- exercicios/js/codigo/4_Desvios_Condicionais/exercicio_19.js
+++ exercicios/js/codigo/4_Desvios_Condicionais/exercicio_19.js
@@ -1,7 +1,48 @@
 //  Desvios Condicionais
 //Exercicio: Numero Inteiro
-//Autor: Seu Nome
+//Autor: Weverton
 
 //=========================| Código |=========================//
+
+
+const mensagem = document.getElementById("mensagem")
+const inputNumero = document.getElementById("numero");
+inputNumero.addEventListener("keyup", (event)=>{
+    if(event.key == "Enter"){
+        verificarNumero();
+    }
+})
+
+function verificarNumero(){
+    if(!inputNumero.value){
+        alert("Insira um Valor!");
+        return;
+    }
+
+    strngNumTamanho = inputNumero.value.length;
+    for(let i = strngNumTamanho; i >= 0; i--){
+        if(inputNumero.value[i] == "." || inputNumero.value[i] == ","){
+            mensagem.style.fontSize = "50px"
+            mensagem.innerHTML = "Isso não é um número inteiro!<br>";
+            setTimeout(() => {
+                mensagem.innerHTML += "Calma ai que eu já te ajudo nisso";
+            }, 1500);
+            setTimeout(() => {
+                window.location.href = "https://www.todamateria.com.br/numeros-inteiros/";
+            }, 3000);
+            return;
+        }
+    }
+    const numero = Number(inputNumero.value);
+    let resp;
+    if(numero > 0){
+        resp = "positivo"
+    } else if (numero < 0){
+        resp = "negativo"
+    } else {
+        resp = "zero"
+    }
+    mensagem.textContent = "O Número informado é " + resp
+}
 
 //============================================================//
