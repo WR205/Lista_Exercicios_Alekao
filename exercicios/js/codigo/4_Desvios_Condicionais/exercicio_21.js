42f693a27b6bb3195ce8b7d9532a0d474f8f351f
WR205 <fastbluestudios2.0@gmail.com>
2025-05-08 18:40:29 -0300

    Commit fazendo exercicio 21 js

--- exercicios/js/codigo/4_Desvios_Condicionais/exercicio_21.js
+++ exercicios/js/codigo/4_Desvios_Condicionais/exercicio_21.js
@@ -4,4 +4,61 @@
 
 //=========================| Código |=========================//
 
+const inputNumero = document.getElementById("numero");
+const resultadoElement = document.getElementById("resultado");
+
+let valores = [];
+let equacao;
+
+
+function calculo(operador){
+    if(!inputNumero.value){
+        alert("Insira um Valor!");
+        return;
+    }
+    resultado()
+    equacao = operador;
+}
+function soma(){
+    valores[0] = valores[0] + valores[1]; 
+}
+function subtracao(){
+    valores[0] = valores[0] - valores[1];  
+}
+function multiplicacao(){
+    valores[0] = valores[0] * valores[1]; 
+}
+function divisao(){
+    valores[0] = valores[0] / valores[1];
+}
+
+function resultado(){
+    valores.push(Number(inputNumero.value));
+    inputNumero.value = "";
+    if(valores[1]){
+        valores[0] = equacao
+        valores.pop;
+    }
+
+
+    console.log(valores);
+    if(valores[1]){
+        switch(equacao){
+            case "+":
+                soma();
+            break;
+            case "-":
+                subtracao();
+            break;
+            case "*":
+                multiplicacao();
+            break;
+            case "/":
+                divisao();
+        }
+        valores.pop;
+        resultadoElement.textContent = valores[0];
+    }
+}
+
 //============================================================//
